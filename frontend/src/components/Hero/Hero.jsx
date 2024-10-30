19de357dfce85758928c85b650653f518fc09994
hayat <hayatullah.ibrah@gmail.com>
2024-10-30 14:40:09 +0100

    look and feel

--- frontend/src/components/Hero/Hero.jsx
+++ frontend/src/components/Hero/Hero.jsx
@@ -11,8 +11,10 @@ function Hero() {
 
         <div className='flex flex-col justify-center gap-1 bg-slate-700 min-h-screen'>
           <div className='max-sm:mt-16 mt-14'>
-            <h1 className='max-sm:text-4xl text-8xl font-bold text-orange-700 uppercase lg:px-8 px-2'>KraftZone</h1>
-            <p className='text-sm text-white lg:px-10 px-3'>Wir entsorgen Ihre Last</p>
+            <h1 className='max-sm:text-4xl text-8xl font-bold text-orange-700 uppercase lg:px-8 px-2'>
+              <span className='text-black'>Kraft</span>Zone
+              </h1>
+            <p className='text-lg text-white lg:px-10 px-3'>Wir entsorgen Ihre Last</p>
           </div>
           <div className='flex justify-center items-center lg:px-10'>
           <Carousel />
